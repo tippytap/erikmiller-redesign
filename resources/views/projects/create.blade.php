b584f448379fb596007ab3d5332609fbea5e6dfc
Erik Miller <emiller46@radford.edu>
2016-10-10 22:24:48 -0400

    added form builder class to project, and created form for creating a project

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                    {!! Form::open(['route' => 'project.create']) !!}
+                        <div class="col-xs-12">
+                            {{ Form::label('title') }}
+                            {{ Form::text('title', '', ['class' => 'form-control']) }}
+                        </div>
+                        <div class="col-xs-12">
+                            {{ Form::label('description') }}
+                            {{ Form::textarea('description', '', ['class' => 'form-control']) }}
+                        </div>
+                        <div class="col-xs-12">
+                            {{ Form::label('link') }}
+                            {{ Form::text('link', '', ['class' => 'form-control']) }}
+                        </div>
+                        <div class="col-xs-12">
+                            {{ Form::label('background') }}
+                            {{ Form::text('background', '', ['class' => 'form-control']) }}
+                        </div>
+                        <div class="col-xs-12">
+                            {{ Form::submit('Create Project', ['class' => 'btn btn-primary']) }}
+                        </div>
+                    {!! Form::close() !!}
+
+            </div>
+        </div>
+    </div>
+@endsection
