239a253d0a9654a1d070d06a4319c875c72b3be3
Erik Miller <emiller46@radford.edu>
2016-11-23 16:21:12 -0500

    updated new button to have correct url

--- resources/views/projects/home.blade.php
+++ resources/views/projects/home.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 @if(Auth::check())
-                    <a href="{{ url('/projects/create') }}" class="btn btn-default"><i class="fa fa-btn fa-plus"></i>New project</a>
+                    <a href="{{ url('/projects/create/new') }}" class="btn btn-default"><i class="fa fa-btn fa-plus"></i>New project</a>
                 @endif
                 @foreach($projects as $project)
                     <h3><a href='{{ url("/projects/$project->title") }}'>{{str_replace("-", " ", $project->title)}}</a></h3>
