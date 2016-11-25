03e48ed8d5ff9b60fe4ff9307bdcb2fa69bdb02a
Erik Miller <emiller@interactivgis.com>
2016-11-25 13:16:50 -0500

    added windows db connection

--- config/database.php
+++ config/database.php
@@ -66,6 +66,20 @@ return [
             'engine' => null,
         ],
 
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'erikmiller-redesign'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
